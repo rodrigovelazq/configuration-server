88cf98dfb3912c849dc360497ac6d3ca43496f0d
rodrigovelazq <rodrigo.24.06.90@gmail.com>
2025-02-21 08:14:09 -0300

    First Commit

---
+++ src/test/java/com/rodrigovelazq/configuration_server/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigovelazq.configuration_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
